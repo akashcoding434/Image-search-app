048a2e74e9a0df0e4e50cf8b909092133f4a6fd8
Akash Kumar <akash12.89097@gmail.com>
2025-02-09 11:11:29 +0530

    Add files via upload
    best image search app

---
+++ image-search-app/index.js
@@ -0,0 +1,37 @@
+let access_key="ezDjYj8VDdT_Py9wT5HAnr8-VMYcmySzMBw3aB3P-LI";
+let formEl=document.querySelector("form");
+let searchEl=document.querySelector(".search-input");
+let serachResults=document.querySelector(".search-results");
+let showMore=document.querySelector(".show-more");
+let page=1;
+const searchImage=async()=>{
+    const searchInput=searchEl.value;
+    const url=`http://api.unsplash.com/search/photos?page=${page}&query=${searchInput}&client_id=${access_key}`;
+    const response=await fetch(url);
+    const data=await response.json();
+    if(page==1){
+        serachResults.innerHTML="";
+    }
+    const result=data.results;
+    result.map((item,index)=>{
+        serachResults.innerHTML+=`
+          <div class="search-result">
+            <img src="${item.urls.small}" alt=""><br> 
+             <a href="${item.links.html}">${item.alt_description}</a>
+        </div> 
+        `
+    });
+    page++;
+    if(page>1){
+        showMore.style.display="block";
+    }
+}
+formEl.onsubmit=(e)=>{
+    e.preventDefault();
+    page=1;
+    searchImage();
+}
+
+showMore.onclick=()=>{
+    searchImage();
+}
